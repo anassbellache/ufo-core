f9b27cb851ae4bb7c11c1464651939a42d6be56e
anassbellache <60012320+anassbellache@users.noreply.github.com>
2025-05-26 12:15:49 +0200

    Implement concurrency tests

--- tests/+ufoTest/TestConcurrency.m
+++ tests/+ufoTest/TestConcurrency.m
@@ -3,13 +3,69 @@ classdef TestConcurrency < matlab.unittest.TestCase
     methods(Test)
         function twoPluginManagers(testCase)
             % CC-01 two MATLAB workers create PluginManager in parallel
-            % TODO: start a parpool with 2 workers and ensure each can
-            % create a ufo.PluginManager without race conditions.
+            pool = gcp('nocreate');
+            hasPCT = ~isempty(ver('parallel'));
+            testCase.assumeTrue(hasPCT, 'Parallel Computing Toolbox required');
+
+            if isempty(pool)
+                pool = parpool(2);
+                cleanup = onCleanup(@() delete(pool)); %#ok<NASGU>
+            elseif pool.NumWorkers < 2
+                delete(pool);
+                pool = parpool(2);
+                cleanup = onCleanup(@() delete(pool)); %#ok<NASGU>
+            end
+
+            f1 = parfeval(pool, @TestConcurrency.createPMOnWorker, 1);
+            f2 = parfeval(pool, @TestConcurrency.createPMOnWorker, 1);
+
+            ok1 = fetchOutputs(f1);
+            ok2 = fetchOutputs(f2);
+
+            testCase.verifyTrue(ok1 && ok2, ...
+                'PluginManager could not be created concurrently');
         end
         function schedulerBusy(testCase)
             % CC-03 ensure Scheduler reports busy when run is called twice
-            % TODO: run Scheduler.runAsync then immediately run again and
-            % verify error id 'ufo:Scheduler:Busy'.
+            pm = ufo.PluginManager();
+            tg = ufo.TaskGraph();
+            a = pm.getTask('read');
+            b = pm.getTask('write');
+            idA = tg.addNode(a);
+            idB = tg.addNode(b);
+            tg.connect(idA, idB);
+            sched = ufo.Scheduler();
+
+            try
+                sched.runAsync(tg);
+            catch ME
+                if strcmp(ME.identifier, 'ufo_mex:NotImplemented')
+                    testCase.assumeFail('Scheduler_runAsync not implemented');
+                else
+                    rethrow(ME);
+                end
+            end
+
+            testCase.verifyError(@() sched.run(tg), 'ufo:Scheduler:Busy');
+
+            if ismethod(sched, 'stop')
+                sched.stop();
+            end
+            delete(sched);
+            delete(tg);
+            delete(pm);
+        end
+    end
+
+    methods(Static, Access = private)
+        function ok = createPMOnWorker
+            try
+                pm = ufo.PluginManager();
+                ok = isvalid(pm);
+                delete(pm);
+            catch
+                ok = false;
+            end
         end
     end
 end
